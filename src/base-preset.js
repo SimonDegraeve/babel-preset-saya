b8815b7d46409d50df6736d6e335ee51af8bc8a6
Simon Degraeve <simon.degraeve@gmail.com>
2016-10-09 13:44:31 +0800

    feat: Add support for export extensions

--- src/base-preset.js
+++ src/base-preset.js
@@ -8,6 +8,7 @@ import transformEs3MemberExpressionLiterals from 'babel-plugin-transform-es3-mem
 import transformEs3PropertyLiterals from 'babel-plugin-transform-es3-property-literals';
 import transformClassProperties from 'babel-plugin-transform-class-properties';
 import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread';
+import transformExportExtensions from 'babel-plugin-transform-export-extensions';
 import transformRegenerator from 'babel-plugin-transform-regenerator';
 import transformRuntimeNodent from 'fast-async';
 
@@ -33,6 +34,7 @@ const preset = {
     // Unstable
     transformClassProperties,
     transformObjectRestSpread,
+    transformExportExtensions,
 
     // Runtime
     [transformRegenerator, {
